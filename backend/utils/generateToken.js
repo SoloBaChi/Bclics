25db06545bd09ae10eaec8ffe7ba68f17941f574
SoloBaChi <solobachi02@gmail.com>
2024-11-29 09:09:53 +0100

    Checking Error

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -10,7 +10,7 @@ const generateToken = (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
     sameSite: 'None',   // Allow cross-origin
     secure: true ,      // Only over HTTPS
-    path:"/",
+    path:"/login",
     domain:"https://bclics-six.vercel.app/"
   });
 
