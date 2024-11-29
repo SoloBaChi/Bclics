4844c8595a44407369eb870bd18036ae83c66b19
SoloBaChi <solobachi02@gmail.com>
2024-11-29 03:45:42 +0100

    CORS issues Fix

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,8 +8,9 @@ const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     httpOnly: true, // more secure
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-    // sameSite: "strict", // CSRF
-    // secure:true
+    sameSite: 'None',   // Allow cross-origin
+    secure: true        // Only over HTTPS
+
   });
 
   return token;
